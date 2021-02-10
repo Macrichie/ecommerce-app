b3a0dcec8af79cb214c1a1f458196ee872145079
Macrichie <makanjuolakunle@gmail.com>
2021-02-10 17:01:32 +0000

    implemented user delete for admin users

--- frontend/src/pages/UserListScreen.js
+++ frontend/src/pages/UserListScreen.js
@@ -1,27 +1,40 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { listUsers } from '../actions/userActions';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteUser, listUsers } from "../actions/userActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 export default function UserListScreen(props) {
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
 
-
+  const userDelete = useSelector((state) => state.userDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = userDelete;
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(listUsers());
+  }, [dispatch, successDelete]);
 
-  }, [dispatch]);
   const deleteHandler = (user) => {
-
+    if (window.confirm("Are you sure to delete?")) {
+      dispatch(deleteUser(user._id));
+    }
   };
   return (
     <div>
       <h1>Users</h1>
-
+      <div className="statusMessage">
+        {loadingDelete && <LoadingBox />}
+        {errorDelete && <MessageBox variant="danger">{errorDelete}</MessageBox>}
+        {successDelete && (
+          <MessageBox variant="success" className="statusMessage__text">User Deleted Successfully</MessageBox>
+        )}
+      </div>
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
@@ -44,8 +57,8 @@ export default function UserListScreen(props) {
                 <td>{user._id}</td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                <td>{user.isSeller ? 'YES' : 'NO'}</td>
-                <td>{user.isAdmin ? 'YES' : 'NO'}</td>
+                <td>{user.isSeller ? "YES" : "NO"}</td>
+                <td>{user.isAdmin ? "YES" : "NO"}</td>
                 <td>
                   <button
                     type="button"
